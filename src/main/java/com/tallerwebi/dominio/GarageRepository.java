2609ec2829012dc0dbdedc990bba11beaa81eeea
Alan <alan.digiovanni@redb.ee>
2024-05-05 19:37:12 -0300

    feat(reservation-confirm): Added garage model, service and repository

---
+++ src/main/java/com/tallerwebi/dominio/GarageRepository.java
@@ -0,0 +1,10 @@
+package com.tallerwebi.dominio;
+
+import com.tallerwebi.dominio.model.Garage;
+
+import java.util.List;
+
+public interface GarageRepository {
+
+    List<Garage> findAll();
+}
---
+++ src/main/java/com/tallerwebi/dominio/GarageService.java
@@ -0,0 +1,11 @@
+package com.tallerwebi.dominio;
+
+import com.tallerwebi.dominio.model.Garage;
+
+import java.util.List;
+
+public interface GarageService {
+
+    List<Garage> getAll();
+
+}
