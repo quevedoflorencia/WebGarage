2cc3221d33f3003e2359feb1fe9851cfcedbafb8
Alan Di Giovanni <adigiovanni@serv.externos.prismamp.com>
2024-05-30 11:06:38 -0300

    fix: Change sign up path

--- src/main/java/com/tallerwebi/presentacion/ControladorLogin.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorLogin.java
@@ -71,5 +71,12 @@ public class ControladorLogin {
         }
         return new ModelAndView("redirect:/login");
     }
+
+    @RequestMapping(path = "/nuevo-usuario", method = RequestMethod.GET)
+    public ModelAndView nuevoUsuario() {
+        ModelMap model = new ModelMap();
+        model.put("usuario", new Usuario());
+        return new ModelAndView("nuevo-usuario", model);
+    }
 }
 
