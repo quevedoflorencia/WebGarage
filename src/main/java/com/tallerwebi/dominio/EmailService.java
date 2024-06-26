a145a5081e6c46fd86cbf517f1ac6f00d7d7f990
Florencia Quevedo <mquevedovaldez@alumno.unlam.edu.ar>
2024-06-25 21:36:34 -0300

    se incopora el logo en el cuerpo del mail y otras mejoras de estilo

--- src/main/java/com/tallerwebi/dominio/EmailService.java
+++ src/main/java/com/tallerwebi/dominio/EmailService.java
@@ -5,7 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 @Service("emailService")
 public class EmailService {
@@ -16,8 +21,40 @@ public class EmailService {
     public EmailService(JavaMailSender emailSender) {
         this.emailSender = emailSender;
     }
-
     public void sendSimpleMessage(Reserva reserva) {
+        MimeMessage message = emailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+            helper.setTo(reserva.getUsuario().getEmail());
+            helper.setSubject("Reserva N°: " + reserva.getId());
+
+            String text = "<html><body>"
+                    + "<img src='cid:logoImage' style=' width: 5%;'><br><br>"
+                    + "Hola " + reserva.getUsuario().getNombre() + ",<br><br>"
+                    + "Aquí están los detalles de tu reserva N° " + reserva.getId() + "<br><br>"
+                    + "Fecha: " + reserva.getDia() + "<br>"
+                    + "Horario de inicio: " + reserva.getHorarioInicio() + "<br>"
+                    + "Horario de finalización: " + reserva.getHorarioFin() + "<br>"
+                    + "Garege: " + reserva.getGarage().getNombre() + "<br>"
+                    + "Mail del Usuario: " + reserva.getUsuario().getEmail() + "<br>"
+                    + "Monto abonado: $" + reserva.getPrecio() + "<br>"
+                    + "Gracias por reservar con WebGarage."
+                    + "</body></html>";
+
+            helper.setText(text, true);
+
+            File archivo = new File("src/main/webapp/resources/core/img/navbar/logo.png");
+            helper.addInline("logoImage", archivo);
+
+            emailSender.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+/*
+    public void sendSimpleMessage(Reserva reserva) {
+
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(reserva.getUsuario().getEmail());
         message.setSubject("Reserva N°: " + reserva.getId());
@@ -29,10 +66,13 @@ public class EmailService {
                 + "Garege: " + reserva.getGarage().getNombre() + "\n"
                 + "Mail del Usuario: " + reserva.getUsuario().getEmail() + "\n"
                 + "Monto abonado: $" + reserva.getPrecio() + "\n"
+                +"<img src= img/navbar/logo.png alt='Imagen de confirmación'/>"
                 + "\n\nGracias por reservar con nosotros.";
+        File file = new File("src/main/webapp/resourses/core/img/navbar/logo.png");
+
         message.setText(text);
         emailSender.send(message);
-    }
+    }*/
 }
 
 
