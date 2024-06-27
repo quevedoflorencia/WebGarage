205fb3f15930ca0d158e8e32179c193192d59d6a
rociomonaco <rociomonaco@gmail.com>
2024-06-26 21:44:28 -0300

    elimino implementacion de metodos

--- src/main/java/com/tallerwebi/dominio/ServicioGarage.java
+++ src/main/java/com/tallerwebi/dominio/ServicioGarage.java
@@ -14,11 +14,6 @@ public interface ServicioGarage {
     List<Garage> getPaginacion(Integer page, Integer size);
     Integer calcularTotalPaginas(Integer totalGarages, Integer size);
     List<Integer> generarNumerosPagina(Integer totalPages);
-    default Integer validarPagina(Integer pagina) {
-        return (pagina == null || pagina <= 0) ? 1 : pagina;
-    }
-
-    default Integer validarTamanioPagina(Integer tamano) {
-        return (tamano == null || tamano <= 0) ? 3 : tamano;
-    }
+    Integer validarPagina(Integer pagina);
+    Integer validarTamanioPagina(Integer tamano);
 }
